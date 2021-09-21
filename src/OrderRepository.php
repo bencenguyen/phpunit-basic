accdb8fa6c5526f4a91d4730e98e7603d6110245
Nguyen Bence <bence325@gmail.com>
2021-09-21 23:40:59 +0200

    feat: create OrderRepository

---
+++ src/OrderRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+class OrderRepository
+{
+
+    private $pdo;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function createOrder(Order $order)
+    {
+
+    }
+
+    public function findORder(int $id)
+    {
+        
+    }
+}
