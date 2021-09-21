a6446cdb6ac4d3f491f11b9bda21d171288925b4
Nguyen Bence <bence325@gmail.com>
2021-09-22 00:11:33 +0200

    feat: add mocked test cases

---
+++ tests/OrderControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests;
+
+use App\CreateOrderRequest;
+use App\OrderController;
+use App\OrderService;
+use PHPUnit\Framework\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+
+    private $underTest;
+
+    private $mockService;
+
+    public function setUp(): void
+    {
+        $this->mockService = $this->createMock(OrderService::class);
+        $this->underTest = new OrderController($this->mockService);
+    }
+
+    /**
+     *  @test
+     */
+    public function it_should_return_fail_when_request_is_not_valid()
+    {
+        $mockRequest = $this->createMock(CreateOrderRequest::class);
+        $mockRequest->method("isValid")->willReturn(false);
+
+        $actual = $this->underTest->placeOrder($mockRequest);
+
+        $this->assertEquals($actual, "fail");
+    }
+
+
+    /**
+     *  @test
+     */
+    public function it_should_return_success_when_request_is_valid()
+    {
+        $mockRequest = $this->createMock(CreateOrderRequest::class);
+        $mockRequest->method("isValid")->willReturn(true);
+
+        $actual = $this->underTest->placeOrder($mockRequest); 
+
+        $this->assertEquals($actual, "success");
+    }
+
+    /**
+     *  @test
+     */
+    public function it_should_call_service_when_request_is_valid()
+    {
+        $mockRequest = $this->createMock(CreateOrderRequest::class);
+        $mockRequest->method("isValid")->willReturn(true);
+        $this->mockService->expects($this->once())->method("placeORder")->with($mockRequest);
+
+        $this->underTest->placeOrder($mockRequest); 
+
+    }
+}
